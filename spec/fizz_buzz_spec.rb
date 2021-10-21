c583c1d010565ebe3dbbbea2b9a4c48ca9b54887
miyeaier <miyesier@gmail.com>
2021-10-21 18:03:35 +0200

    aolves the challenge

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -8,15 +8,15 @@ describe FizzBuzz do
    expect(subject.check(1)).to eq 1
  end
 
- xit "is expected to return 'fizz' if the number is divisable by 3" do
+ it "is expected to return 'fizz' if the number is divisable by 3" do
   expect(subject.check(3)).to eq "fizz"
  end
 
- xit "is expected to return 'buzz' if the number is divisable by 5" do
+ it "is expected to return 'buzz' if the number is divisable by 5" do
   expect(subject.check(5)).to eq "buzz"
  end
 
- xit "is expected to return 'fizz-buzz' if the number is divisable by 15" do
+ it "is expected to return 'fizz-buzz' if the number is divisable by 15" do
   expect(subject.check(15)).to eq "fizz-buzz"
  end
 
--- src/fizz_buzz.rb
+++ src/fizz_buzz.rb
@@ -1,7 +1,19 @@
 class FizzBuzz
 
  def check(value)
-   return value
- end 
+
+    if value % 15 == 0
+     return "fizz-buzz"
+    
+    elsif value % 3 == 0
+      return "fizz"
+   
+    elsif value % 5 == 0
+    return "buzz"
+    
+    else
+      return value
+    end 
+  end
 
 end
